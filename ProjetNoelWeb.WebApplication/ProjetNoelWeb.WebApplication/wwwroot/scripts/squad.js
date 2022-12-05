fa0d50e5f0ffff9e232664914ccaff1d4a7eb5fd
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-12-05 19:51:56 +0100

    Modif pour enlever le déplacement des idées

--- ProjetNoelWeb.WebApplication/ProjetNoelWeb.WebApplication/wwwroot/scripts/squad.js
+++ ProjetNoelWeb.WebApplication/ProjetNoelWeb.WebApplication/wwwroot/scripts/squad.js
@@ -1,73 +1,4 @@
-﻿// function slist() {
-//    var target = document.getElementById("sortlist");
-//    target.classList.add("slist");
-//    //let items = target.querySelectorAll("li.QuestionInstruction");
-//    let items = target.getElementsByTagName("li");
-
-//    current = null;
-
-//    var index = 0;
-//    // (B) MAKE ITEMS DRAGGABLE + SORTABLE
-//    for (let i of items) {
-//        // (B1) ATTACH DRAGGABLE
-//        index++;
-//        i.draggable = true;
-
-//        // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
-//        i.ondragstart = (ev) => {
-//            current = i;
-//        };
-
-//        // When we passed with item in mouse on other item
-//        i.ondragenter = (ev) => {
-//            if (i != current) {
-//                i.classList.add("active");
-//            }
-//        };
-
-//        // When we passed with item in mouse on other item
-//        i.ondragleave = () => {
-//            i.classList.remove("active");
-//        };
-
-//        // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
-//        i.ondragend = () => {
-//            for (let it of items) {
-//                it.classList.remove("active");
-//            }
-//        };
-
-//        // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
-//        i.ondragover = (evt) => {
-//            evt.preventDefault();
-//        };
-
-//        // (B7) ON DROP - DO SOMETHING
-//        i.ondrop = (evt) => {
-//            evt.preventDefault();
-//            if (i != current) {
-//                let currentpos = 0,
-//                    droppedpos = 0;
-//                for (let it = 0; it < items.length; it++) {
-//                    if (current == items[it]) {
-//                        currentpos = it;
-//                    }
-//                    if (i == items[it]) {
-//                        droppedpos = it;
-//                    }
-//                }
-//                if (currentpos < droppedpos) {
-//                    i.parentNode.insertBefore(current, i.nextSibling);
-//                } else {
-//                    i.parentNode.insertBefore(current, i);
-//                }
-//            }
-//        };
-//    }
-
-//}
-
-function addInput(name) {
+﻿function addInput(name) {
     event.preventDefault();
     var ul = document.getElementById('sortlist'); // get the list 
     var i = document.createElement('i'); // create i 
@@ -134,20 +65,8 @@ function addInput(name) {
     li.appendChild(inputPrice);
     li.appendChild(i);
 
-
-    // If it's a introduction field
-    if (name == 'Introduction') {
-        // Check if there is already one intruction field
-        // var firstIntroduction = document.querySelector("input[value='Introduction']");
-
-        var firstChild = ul.childNodes[0];
-        ul.insertBefore(li, firstChild);
-    }
-    else {
-        var lastChild = ul.children[ul.children.length - 1];
-        ul.insertBefore(li, lastChild);
-    }
-    slist();
+    var lastChild = ul.children[ul.children.length - 1];
+    ul.insertBefore(li, lastChild);
 }
 
 function deleteInput(caller) {
